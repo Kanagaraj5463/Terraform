3436cbe4f08a0d5c805d53d88e7b1d0d903ad047
Kanagaraj A <46182031+Kanagaraj5463@users.noreply.github.com>
2021-11-14 14:57:28 +0530

    Update provisioner.tf

--- provisioner.tf
+++ provisioner.tf
@@ -26,3 +26,9 @@ resource "aws_security_group" "websg" {
     cidr_blocks = [ "0.0.0.0/0" ]
   }
 }
+resource "aws_key_pair" "gokule" {
+  key_name   = "gokule"
+  public_key = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQC2ufU1ZjPzfhaqmSwKhxMXbjatIQzFKAaf62OYeH6yU7n7K91I/oVkg/BqR36r3cRk1DP72Sdo3Gsd0Q6zB6buExCJRuSgHM9UgDHmivwJIOHEXsoD5RSUaiLhDcGLPTiO0BVrZ6hz
+qhzsaMgHiS96ewA14r20ECN4PR1cZoV4xxAS6K8XllwRczygz+9i0Coodw4pOQaespxNq9mie/p2QsndS8FAkGFCO8JxhUQdA9S+tNHk+ZA/XPWgHXfqhYglkv2FnMKbumls/FdAx0wEweipAH4tuyh8tmw9Tkpmv4f+vbJeEPcILSsn
+VCt8bW8NGsevGzyIvEPckKuCY9mV Gokule"
+}
