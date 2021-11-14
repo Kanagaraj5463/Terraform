4b150fb7a6a3b4c767e06bced1e16dddd30c9448
Kanagaraj A <46182031+Kanagaraj5463@users.noreply.github.com>
2021-11-14 15:38:59 +0530

    Update provisioner.tf

--- provisioner.tf
+++ provisioner.tf
@@ -7,6 +7,7 @@ resource "aws_instance" "tfvm" {
   count = 2
   ami = "ami-0f1fb91a596abf28d"
   instance_type = "t2.micro"
+  key_name = "Gokule"
   vpc_security_group_ids = [ aws_security_group.websg.id ]
   user_data = <<-EOF
                 #!/bin/bash
@@ -14,7 +15,7 @@ resource "aws_instance" "tfvm" {
                 nohup busybox httpd -f -p 8080 &
                 EOF
     tags = {
-      Name = "Web-app"
+      Name = "Auto-VM"
     }
 }
 resource "aws_security_group" "websg" {
@@ -26,9 +27,4 @@ resource "aws_security_group" "websg" {
     cidr_blocks = [ "0.0.0.0/0" ]
   }
 }
-resource "aws_key_pair" "gokule" {
-  key_name   = "gokule"
-  public_key = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQC2ufU1ZjPzfhaqmSwKhxMXbjatIQzFKAaf62OYeH6yU7n7K91I/oVkg/BqR36r3cRk1DP72Sdo3Gsd0Q6zB6buExCJRuSgHM9UgDHmivwJIOHEXsoD5RSUaiLhDcGLPTiO0BVrZ6hz
-qhzsaMgHiS96ewA14r20ECN4PR1cZoV4xxAS6K8XllwRczygz+9i0Coodw4pOQaespxNq9mie/p2QsndS8FAkGFCO8JxhUQdA9S+tNHk+ZA/XPWgHXfqhYglkv2FnMKbumls/FdAx0wEweipAH4tuyh8tmw9Tkpmv4f+vbJeEPcILSsn
-VCt8bW8NGsevGzyIvEPckKuCY9mV Gokule"
-}
+
